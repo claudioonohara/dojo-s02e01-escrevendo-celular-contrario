956506bc1948144781889a597c417e0f11fc653d
claudio <claudioonohara@gmail.com>
2016-08-20 18:52:18 -0400

    ta funcionando

--- src/DiscagemFrase.rb
+++ src/DiscagemFrase.rb
@@ -5,12 +5,33 @@ class DiscagemFrase
   end
 
   def disca(frase)
-
-     frase.each_char do |char|
-        if  char =~ /a|b|c/
-          @resultado = 2
-        end
-     end
+   frase.downcase!
+   frase.gsub!(" ", "")
+   frase.each_char do |char|
+    if char =~ /a|b|c/
+       @resultado << '2'
+    elsif char =~ /d|e|f/
+       @resultado << '3'
+    elsif char =~ /g|h|i/
+       @resultado << '4'
+    elsif char =~ /j|k|l/
+       @resultado << '5'
+    elsif char =~ /m|n|o/
+       @resultado << '6'
+    elsif char =~ /p|q|r|s/
+       @resultado << '7'
+    elsif char =~ /t|u|v/
+       @resultado << '8'
+    elsif char =~ /w|x|y|z/
+       @resultado << '9'
+    elsif char == '-'
+      @resultado << '-'
+    elsif char =~ /[0-9#*]/
+      @resultado << char
+    else
+      raise ArgumentError.new("fiiiii isso nao existe no teclado do telefone!!")
+    end
+   end
      @resultado
   end
 
--- src/teste_de_discagem_spec.rb
+++ src/teste_de_discagem_spec.rb
@@ -8,11 +8,24 @@ describe DiscagemFrase do
         @telefone = DiscagemFrase.new()
     end
 
+    it "1-HOME-SWEET-HOME deve discar 1-4663-79338-4663" do
+      assert_equal('1-4663-79338-4663',@telefone.disca('1-HOME-SWEET-HOME'))
+    end
 
-    it "deve ser 2 quando entrada for a,b ou c" do
-      assert_equal(2,@telefone.disca('a'))
-      assert_equal(2,@telefone.disca('b'))
-      assert_equal(2,@telefone.disca('c'))
+    it "MY-MISERABLE-JOB deve discar 69-647372253-562" do
+      assert_equal('69-647372253-562',@telefone.disca('MY-MISERABLE-JOB'))
+    end
+
+    it "The quick brown fox jumps over the lazy dog deve discar 84378425276963695867768378435299364" do
+       assert_equal('84378425276963695867768378435299364',@telefone.disca('the quick brown fox jumps over the lazy dog'))
+    end
+
+    it "Deve ser lançada uma exception quando a entrada for invalida" do
+        assert_raises( ArgumentError,"fiiiii isso nao existe no teclado do telefone!!") { @telefone.disca('}}') }
+    end
+
+    it "Caracteres especiais # e * devem passar" do
+        assert_equal('69-#47372253-56*',@telefone.disca('MY-#ISERABLE-JO*'))
     end
 
 
